e9497dde60f509d29b536b449011902f754c0d8e
M4asl <mateusz.maslowiec98@gmail.com>
2022-10-15 14:32:22 +0200

    Post Screen

---
+++ src/screens/PostScreen/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const PostScreen = () => {
+  return (
+    <SafeAreaView>
+      
+      <Text>PostScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default PostScreen
