8c9ea4fe9e6e8635c0b2aaa10c0b0c2838da30f2
M4asl <mateusz.maslowiec98@gmail.com>
2022-10-17 21:34:30 +0200

    Create another screens

---
+++ src/screens/DiscoveryScreen/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const DiscoveryScreen = () => {
+  return (
+    <SafeAreaView>
+
+      <Text>DiscoveryScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default DiscoveryScreen
---
+++ src/screens/NotificationsScreen/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const NotificationsScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>NotificationsScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default NotificationsScreen
